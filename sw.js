783a079ab34d92d9b5a9a865c425d68de5a5a827
kakiboy <wiroj11keng@gmail.com>
2026-09-14 09:37:42 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'smart-note-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// ติดตั้ง Service Worker และแคชไฟล์
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// ดึงข้อมูลจากแคชเมื่อไม่มีอินเทอร์เน็ต
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
